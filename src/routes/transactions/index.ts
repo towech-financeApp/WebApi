48d85668b90dd01f76b623bcdfbbbc7ce3ac7856
Jose Tow <jose.towe@gmail.com>
2022-04-21 18:11:02 -0500

    Added exclude report capability to transactions

--- src/routes/transactions/index.ts
+++ src/routes/transactions/index.ts
@@ -41,6 +41,7 @@ transactionRoutes.post('/', middlewares.checkConfirmed, async (req, res) => {
         concept: req.body.concept,
         amount: req.body.amount,
         transactionDate: req.body.transactionDate,
+        excludeFromReport: req.body.excludeFromReport,
       } as Objects.Transaction,
     });
 
